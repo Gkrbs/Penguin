fff81f0f17b6250ee7d9bcf75a3f858d24188704
sminhyeong <96801506+sminhyeong@users.noreply.github.com>
2024-02-28 10:49:28 +0900

    aimObject스크립트 추가 Commit

---
+++ Assets/Script/Utill/AimObject.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimObject : MonoBehaviour
+{
+    [SerializeField]
+    float _move_speed = 10.0f;
+    [SerializeField]
+    private Transform _fire_point_tr;
+    [SerializeField]
+    private HookGun _gun;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, _fire_point_tr.position) < (_gun.MAX_DISTANCE - 0.5f))
+        {
+            transform.Translate(Vector3.forward*Time.deltaTime * _move_speed);
+        }
+        else if(Vector3.Distance(transform.position, _fire_point_tr.position) > (_gun.MAX_DISTANCE + 0.5f))
+        {
+            transform.Translate(Vector3.forward*Time.deltaTime * -_move_speed);
+
+        }
+    }
+}
