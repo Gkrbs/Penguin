657eea64ccd01ad334c68de117b63cb837b18e9c
sminhyeong <96801506+sminhyeong@users.noreply.github.com>
2024-03-19 15:03:16 +0900

    Revert "UI매니저 수정"
    This reverts commit 97581c849d0650ff74a231d4c0aa7b7022bbed2a.

--- Assets/Script/UI/UIManager.cs
+++ Assets/Script/UI/UIManager.cs
@@ -22,8 +22,6 @@ public class UIManager : MonoBehaviour
     private GameObject _jetpack_img, _create_wall_img;
     [SerializeField]
     private NormalMovement nm;
-    [SerializeField]
-    private KeyCode code;
 
     private void Start()
     {
@@ -33,7 +31,7 @@ public class UIManager : MonoBehaviour
 
     private void VisibleMenu()
     {
-        if (Input.GetKeyDown(code))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (_open_menu)
             {
@@ -49,14 +47,12 @@ public class UIManager : MonoBehaviour
                     Cursor.visible = false;
                     _menu_panel.SetActive(false);
                     Time.timeScale = 1.0f;
-                    _open_menu = false;
                 }
             }
             else
             {
                 if (!_menu_panel.activeSelf)
                 {
-                    _open_menu = true;
                     Cursor.lockState = CursorLockMode.Confined;
                     Cursor.visible = true;
                     _menu_panel.SetActive(true);
