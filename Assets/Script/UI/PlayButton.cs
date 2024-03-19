9db0f68742c62f007dd8207e99a5ccd2fa1fe548
sminhyeong <96801506+sminhyeong@users.noreply.github.com>
2024-03-19 10:18:58 +0900

    title에서 로드 scnen되도록 작업 후 커밋

--- Assets/Script/UI/PlayButton.cs
+++ Assets/Script/UI/PlayButton.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayButton : ButtonControl
 {
+    [SerializeField]
+    private Slider _loading_bar;
+    [SerializeField]
+    private GameObject _loading_panel;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +18,28 @@ public class PlayButton : ButtonControl
     {
         base.OnClickedEvent();
         SoundManager.instance.PlayOneShot(_audio, "StartAnimation");
-        SceneManager.LoadScene("PlayScen");
+        StartCoroutine(LoadingScene());
 
     }
-    // Update is called once per frame
-    void Update()
+
+    IEnumerator LoadingScene()
     {
+        AsyncOperation operation =  SceneManager.LoadSceneAsync("PlayScene");
+        _loading_panel.SetActive(true);
         
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        while (!operation.isDone)
+        {
+            float progress_value = Mathf.Clamp01(operation.progress / 0.9f);
+            _loading_bar.value = progress_value;
+
+            yield return null;
+                
+        }
+
     }
+
+
 }
