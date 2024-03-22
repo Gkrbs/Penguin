d0b5b9ed5f1be0afac201ad632fd71fae8ecc78c
pkh555 <134608073+changeNameTo@users.noreply.github.com>
2024-03-22 18:07:29 +0900

    1스테이지 수정중(1-3 위치 및 패턴 수정)

--- Assets/Script/Item/Item.cs
+++ Assets/Script/Item/Item.cs
@@ -8,12 +8,6 @@ namespace Lightbug.CharacterControllerPro.Demo
 {
     public class Item : CharacterDetector
     {
-        [SerializeField]
-        private bool itemActiveState = true;
-        [SerializeField]
-        private float respawnTime = 60f;
-        [SerializeField]
-        private float elapsedTime = 0f;
         public enum ITEM_TYPE
         {
             JET_PACK,
@@ -24,26 +18,7 @@ namespace Lightbug.CharacterControllerPro.Demo
         protected override void ProcessEnterAction(CharacterActor characterActor)
         {
             characterActor.GetComponentInBranch<ItemCheck>().ItemGet(currentItemType);
-            ItemActive(false);
-        }
-        private void Update()
-        {
-            if (!itemActiveState)
-            {
-                elapsedTime += Time.deltaTime;
-                if(elapsedTime >= respawnTime)
-                {
-                    ItemActive(true);
-                    elapsedTime = 0;
-                }
-            }
-        }
-
-        void ItemActive(bool set)
-        {
-            gameObject.GetComponent<MeshRenderer>().enabled = set;
-            gameObject.GetComponent<BoxCollider>().enabled = set;
-            itemActiveState = set;
+            gameObject.SetActive(false);
         }
     }
 }
