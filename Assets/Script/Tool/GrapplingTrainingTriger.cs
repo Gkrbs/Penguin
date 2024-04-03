1d764da0da3b7bd239a95ed800e7647dc353e19f
sminhyeong <96801506+sminhyeong@users.noreply.github.com>
2024-04-03 18:45:53 +0900

    케그 및 레이어 추가후 그레플링 트레이닝룸 만들어 커밋

---
+++ Assets/Script/Tool/GrapplingTrainingTriger.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingTrainingTriger : MonoBehaviour
+{
+    public bool is_trigger = false;
+    public string equals_tag = "";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals(equals_tag))
+            is_trigger = true;
+    }
+}
---
+++ Assets/Script/Tool/GrapplingTrainngRoom.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GrapplingTrainngRoom : MonoBehaviour
+{
+    private float _max_dist = 0f;
+    [SerializeField]
+    private TMP_Text _point_text;
+    [SerializeField]
+    private Transform _start_pos;
+    [SerializeField]
+    private Transform _end_pos;
+    [SerializeField]
+    private GrapplingTrainingTriger triger;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _max_dist = Vector3.Distance(_end_pos.position, _start_pos.position); 
+        _point_text.text = "";
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!triger.is_trigger) return;
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            triger.is_trigger = false;
+            float currer_dist = Vector3.Distance(new Vector3(_start_pos.position.x,
+                _start_pos.position.y,
+                other.transform.position.z), _start_pos.position);
+            _point_text.text = ((int)( currer_dist/ _max_dist*100f)).ToString() + "POINT";
+        }
+    }
+}
