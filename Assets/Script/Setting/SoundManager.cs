5db6b0138ce24e44453e51741cd02627a054027e
sminhyeong <96801506+sminhyeong@users.noreply.github.com>
2024-04-04 16:35:30 +0900

    사운드메니저 print함수제거

--- Assets/Script/Setting/SoundManager.cs
+++ Assets/Script/Setting/SoundManager.cs
@@ -34,7 +34,6 @@ public class SoundManager : MonoBehaviour
     {
         AudioClip clip = null;
         clipsDict.TryGetValue(name, out clip);
-        Debug.Log(name);
         if (clip == null)
         {
             Debug.Log("no clip name");
