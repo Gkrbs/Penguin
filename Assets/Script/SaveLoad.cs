def8b52b009e49a559631086cbc92862ffa0a5cd
Gkrbs <marsg6926@gmail.com>
2024-04-09 12:30:39 +0900

    세이브 메세지 추가 플레이씬 업로드

--- Assets/Script/SaveLoad.cs
+++ Assets/Script/SaveLoad.cs
@@ -8,13 +8,16 @@ public class SaveLoad : MonoBehaviour
     Vector3 position;
     public CharacterActor characterActor;
     public GameObject player;
+    public Animator anim;
     // Update is called once per frame
     void Update()
     {
-        if(GameManager.instance.SELECTED_LEVEL == GameManager.LEVELS.NORMAL) gameObject.SetActive(false);
+        if (GameManager.instance == null) return;
+        if (GameManager.instance.SELECTED_LEVEL == GameManager.LEVELS.NORMAL) gameObject.SetActive(false);
 
         if(Input.GetKeyDown(KeyCode.T) && characterActor.IsGrounded)
         {
+            anim.SetTrigger("Save");
             position = player.transform.position;
         }
 
