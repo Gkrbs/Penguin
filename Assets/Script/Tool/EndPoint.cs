deed930d5d4c8804c46f1b24efe9bd0435e31197
sminhyeong <96801506+sminhyeong@users.noreply.github.com>
2024-04-11 11:19:42 +0900

    엔딩 포인트 정상 작동안돼 수정하여 커밋

--- Assets/Script/Tool/EndPoint.cs
+++ Assets/Script/Tool/EndPoint.cs
@@ -10,19 +10,22 @@ public class EndPoint : MonoBehaviour
     private string SceneName = "";
     [SerializeField]
     private GameObject _fadeout_panel;
-    
+
     private Animation _fadeout_ani;
-    
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-                GameManager.instance?.SelectLevel(GameManager.LEVELS.NONE);
-                Timer.instance?.StopTimer();
+            GameManager.instance?.SelectLevel(GameManager.LEVELS.NONE);
+            Timer.instance?.StopTimer();
+            if (_fadeout_panel != null)
+            {
                 _fadeout_panel?.SetActive(true);
                 _fadeout_ani ??= _fadeout_panel.GetComponent<Animation>();
                 _fadeout_ani?.Play();
-                _collision_player = true;
+            }
+            _collision_player = true;
         }
     }
     private void Update()
