31b90ad3cd4d6fb7cc91643e62f115c63d5f564b
Gkrbs <marsg6926@gmail.com>
2024-05-29 15:13:47 +0900

    엘레베이터 수정

--- Assets/Script/Elevator/ElavatorSoundEvent.cs
+++ Assets/Script/Elevator/ElavatorSoundEvent.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class ElavatorSoundEvent : MonoBehaviour
 {
     public AudioSource audio;
-
+    public GameObject upStairDoor;
+    public GameObject downStairDoor;
+    public ElevatorIsUpCheck elevatorIsUpCheck;
     public void MoveElevatorStart()
     {
         SoundManager.instance.PlayOneShot(audio, "ElevatorMovingStart");
@@ -23,9 +25,20 @@ public class ElavatorSoundEvent : MonoBehaviour
     public void CloseDoor()
     {
         SoundManager.instance.PlayOneShot(audio, "ElevatorDoor");
+        StartCoroutine(CloseDoorSetFalse());
     }
     public void OpenDoor()
     {
         SoundManager.instance.PlayOneShot(audio, "ElevatorDoor",4);
+        if (!elevatorIsUpCheck.isUp)
+            upStairDoor.SetActive(false);
+        else
+            downStairDoor.SetActive(false);
+    }
+    IEnumerator CloseDoorSetFalse()
+    {
+        yield return new WaitForSeconds(3f);
+        upStairDoor.SetActive(true);
+        downStairDoor.SetActive(true);
     }
 }
